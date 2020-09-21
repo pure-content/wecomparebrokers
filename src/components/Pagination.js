c8e22133732229e71aa51b25e4e9917c7ce71c3c
Valik <osiptsev.valik@gmail.com>
2020-09-21 12:22:26 +0300

    Pagination fixed

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -10,13 +10,18 @@ export default function Pagination({ postsPerPage, totalPosts, currentPage, setC
         let prevPage = currentPage > 1 ? currentPage - 1 : null
         let nextPage = currentPage < pageNumbers.length ? currentPage + 1 : null
         let lastPage = pageNumbers.length
+        let firstPage = 1
 
         return (
             <ul class="pagination">
                 {currentPage > 1 ? <li><a class="prev pagination" onClick={() => setCurrentPage(currentPage--)}><i class="fa fa-angle-left" aria-hidden="true"></i> Previous</a></li> : null}
+                {currentPage > 3 ? <li><a class='pagination' onClick={() => setCurrentPage(firstPage)}>{firstPage}</a></li> : null}
+                {currentPage > 3 ? <li><span class="pagination dots">…</span></li> : null}
+
                 {prevPage ? <li><a class='pagination' onClick={() => setCurrentPage(prevPage)}>{prevPage}</a></li> : null}
                 <li><span aria-current="page" class="pagination current">{currentPage}</span></li>
                 {nextPage ? <li><a class='pagination' onClick={() => setCurrentPage(nextPage)}>{nextPage}</a></li> : null}
+
                 {currentPage !== lastPage ? <li><span class="pagination dots">…</span></li> : null}
                 {currentPage !== lastPage ? <li><a class='pagination' onClick={() => setCurrentPage(lastPage)}>{lastPage}</a></li> : null}
                 {currentPage !== lastPage ? <li><a class="next pagination" onClick={() => setCurrentPage(currentPage++)} >Next <i class="fa fa-angle-right" aria-hidden="true"></i></a></li> : null}
