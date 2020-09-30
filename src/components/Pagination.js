11bdbe0bf2572d356231896d05e9f986846074b6
Valik <osiptsev.valik@gmail.com>
2020-09-30 14:50:09 +0300

    pagination fixes

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -14,35 +14,35 @@ export default function Pagination({ postsPerPage, totalPosts, currentPage, setC
 
         return (
             <ul class="pagination">
-                {currentPage > 1 ? <li><a class="prev pagination" onClick={() => setCurrentPage(currentPage--)}><i class="fa fa-angle-left" aria-hidden="true"></i> Previous</a></li> : null}
-                {currentPage > 3 ? <li><a class='pagination' onClick={() => setCurrentPage(firstPage)}>{firstPage}</a></li> : null}
+                {currentPage > 1 ? <li><a class="prev pagination" onClick={() => setCurrentPage(--currentPage)}><i class="fa fa-angle-left" aria-hidden="true"></i> Previous</a></li> : null}
+                {currentPage >= 3 ? <li><a class='pagination' onClick={() => setCurrentPage(firstPage)}>{firstPage}</a></li> : null}
                 {currentPage > 3 ? <li><span class="pagination dots">…</span></li> : null}
 
                 {prevPage ? <li><a class='pagination' onClick={() => setCurrentPage(prevPage)}>{prevPage}</a></li> : null}
                 <li><span aria-current="page" class="pagination current">{currentPage}</span></li>
                 {nextPage ? <li><a class='pagination' onClick={() => setCurrentPage(nextPage)}>{nextPage}</a></li> : null}
 
-                {currentPage !== lastPage ? <li><span class="pagination dots">…</span></li> : null}
-                {currentPage !== lastPage ? <li><a class='pagination' onClick={() => setCurrentPage(lastPage)}>{lastPage}</a></li> : null}
-                {currentPage !== lastPage ? <li><a class="next pagination" onClick={() => setCurrentPage(currentPage++)} >Next <i class="fa fa-angle-right" aria-hidden="true"></i></a></li> : null}
+                {currentPage < lastPage - 2 ? <li><span class="pagination dots">…</span></li> : null}
+                {currentPage < lastPage - 1 ? <li><a class='pagination' onClick={() => setCurrentPage(lastPage)}>{lastPage}</a></li> : null}
+                {currentPage !== lastPage ? <li><a class="next pagination" onClick={() => setCurrentPage(++currentPage)} >Next <i class="fa fa-angle-right" aria-hidden="true"></i></a></li> : null}
             </ul>
         )
     }
     if (noNumbers) {
         return (
             <>
-                {currentPage > 1 ? <a class="prev pagination" onClick={() => setCurrentPage(currentPage--)}> Previous</a> : <span class="prev">Previous</span>}
-                {currentPage !== pageNumbers.length ? <a class="next pagination" onClick={() => setCurrentPage(currentPage++)} >Next </a> : <span class="next">Next</span>}
+                {currentPage > 1 ? <a class="prev pagination" onClick={() => setCurrentPage(--currentPage)}> Previous</a> : <span class="prev">Previous</span>}
+                {currentPage !== pageNumbers.length ? <a class="next pagination" onClick={() => setCurrentPage(++currentPage)} >Next </a> : <span class="next">Next</span>}
             </>
         )
     }
     return (
         <ul class="pagination">
-            {currentPage > 1 ? <li><a class="prev pagination" onClick={() => setCurrentPage(currentPage--)}><i class="fa fa-angle-left" aria-hidden="true"></i> Previous</a></li> : null}
+            {currentPage > 1 ? <li><a class="prev pagination" onClick={() => setCurrentPage(--currentPage)}><i class="fa fa-angle-left" aria-hidden="true"></i> Previous</a></li> : null}
             {pageNumbers.map(number => {
                 return <li key={number}>{currentPage === number ? <span aria-current="page" class="pagination current">{number}</span> : <a class='pagination' onClick={() => setCurrentPage(number)}>{number}</a>}</li>
             })}
-            {currentPage !== pageNumbers.length ? <li><a class="next pagination" onClick={() => setCurrentPage(currentPage++)} >Next <i class="fa fa-angle-right" aria-hidden="true"></i></a></li> : null}
+            {currentPage !== pageNumbers.length ? <li><a class="next pagination" onClick={() => setCurrentPage(++currentPage)} >Next <i class="fa fa-angle-right" aria-hidden="true"></i></a></li> : null}
         </ul>
     )
 }
