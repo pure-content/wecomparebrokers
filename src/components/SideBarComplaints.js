d480f8c0a5890620f9620ea91ee7ce1ab8eab899
Valik <osiptsev.valik@gmail.com>
2020-10-13 13:40:28 +0300

    QandA single almost done

--- src/components/SideBarComplaints.js
+++ src/components/SideBarComplaints.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link, useStaticQuery, graphql } from "gatsby"
 
-export default function SideBarComplaints() {
+export default function SideBarComplaints({ complainInfo }) {
     const sidebarInfo = useStaticQuery(graphql`
     query {
         wpgraphql {
@@ -31,25 +31,50 @@ export default function SideBarComplaints() {
     const { sidebarPopularBrokersList } = sidebarInfo.wpgraphql.page.tmplComplaintsArchivePage
 
     return (
-        <aside class="widget popular-brok-widget">
-            <h3>Most popular brokers</h3>
-            <div class="dot-sep">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            {sidebarPopularBrokersList ? (
-                <ul>
-                    {sidebarPopularBrokersList.map((brok) => (
-                        <li>
-                            <Link to={brok.broker.uri}>
-                                {brok.broker.featuredImage ? <span class="logo-wrap"><img src={brok.broker.featuredImage.node.mediaItemUrl} /></span> : null}
-                                {brok.alternativeBrokerName ? <h3>{brok.alternativeBrokerName}</h3> : <h3>{brok.broker.title}</h3>}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
+        <>
+            {complainInfo ? (
+                <aside class="widget comp-information">
+                    <h3>Complaint Info</h3>
+                    <div class="dot-sep">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    {complainInfo.assignedBroker ? (
+                        <div class="broker-data comp-info-wrap">
+                            <h6>Disputed casino</h6>
+                            <p>{complainInfo.assignedBroker.title}</p>
+                        </div>
+                    ) : null}
+                    {complainInfo.assignedBroker ? (
+                        <div class="reason-data comp-info-wrap">
+                            <h6>Reason</h6>
+                            <p>{complainInfo.complaintReason}</p>
+                        </div>
+                    ) : null}
+                </aside>
             ) : null}
-        </aside>
+
+            <aside class="widget popular-brok-widget">
+                <h3>Most popular brokers</h3>
+                <div class="dot-sep">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                {sidebarPopularBrokersList ? (
+                    <ul>
+                        {sidebarPopularBrokersList.map((brok) => (
+                            <li>
+                                <Link to={brok.broker.uri}>
+                                    {brok.broker.featuredImage ? <span class="logo-wrap"><img src={brok.broker.featuredImage.node.mediaItemUrl} /></span> : null}
+                                    {brok.alternativeBrokerName ? <h3>{brok.alternativeBrokerName}</h3> : <h3>{brok.broker.title}</h3>}
+                                </Link>
+                            </li>
+                        ))}
+                    </ul>
+                ) : null}
+            </aside>
+        </>
     )
 }
--- src/customPostTypes/QandASingle.js
+++ src/customPostTypes/QandASingle.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 import Layout from '../components/layout'
 import Parser from "html-react-parser"
 import Helmet from "react-helmet"
+import SideBarComplaints from '../components/SideBarComplaints'
 
 
 export const query = graphql`
@@ -12,6 +13,8 @@ export const query = graphql`
                 content
                 title
                 uri
+                status
+
                 cptQuestion {
                     accountName
                     assignedBroker {
@@ -24,7 +27,19 @@ export const query = graphql`
                     complaintReason
                     userEmail
                 }
+
+                postmeta {
+                    resolved
+                }
+
+                seo {
+                    metaDesc
+                    title
+                    opengraphType
+                }
+
             }
+
         }
     }
 `
@@ -32,8 +47,68 @@ export const query = graphql`
 export default function QandASingle({ data }) {
     const { qa } = data.wpgraphql
     console.log(qa)
+
+    const TopContent = () => {
+        return (
+            <div class="top-content-wrap find-page">
+                <div class="row top-content fraud-top">
+                    <div class="small-12 columns">
+                        <div class="crumbs">
+                            <Link to={'/'}>Home page</Link> -&gt;
+                    <Link to={'/answers'}>Complains</Link> -&gt;
+                    <span> {qa.title} </span>
+                        </div>
+                        <article>
+                            <h3 class="page_title">
+                                <img src='https://meek-hint.flywheelsites.com/wp-content/uploads/2019/03/fraud.svg' alt="Title" />
+                             Having trouble with a Brokers? <strong>We’re here to help!</strong>
+                            </h3>
+                            <div class="dot-sep">
+                                <span></span>
+                                <span></span>
+                                <span></span>
+                            </div>
+                        </article>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    const MainColumn = () => {
+        return (
+            <div class="large-9 medium-8 columns">
+                <div class="cma-comp-list-wrap">
+                    {qa.status !== 'publish' ? <div class="msg-notification"><h6>Your complaint has been submited. Your message is being held for moderation and will be published when gets approval.</h6></div> : null}
+                    <div class="msg-title">
+                        <h1>{qa.title}</h1>
+                        <span class={`comp-stat ${qa.postmeta.resolved}`}>{qa.postmeta.resolved}</span>
+                    </div>
+                    <div class="msg-wrap">
+
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <Layout>
+            <Helmet
+                htmlAttributes={{ lang: "en", amp: undefined }}
+                title={qa.seo.title}
+                meta={[
+                    { name: "description", content: qa.seo.metaDesc },
+                    { property: "og:type", content: qa.seo.opengraphType },
+                ]}
+            />
+            <TopContent />
+            <div class="row cma-wrap">
+                <MainColumn />
+                <div class="large-3 medium-4 columns sidebar">
+                    <SideBarComplaints complainInfo={qa.cptQuestion} />
+                </div>
+            </div>
 
         </Layout>
     )
